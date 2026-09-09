20eba66ab654b27b376a0044a97f0148fbaaadcf
mhu <marcel.huismann@protonmail.ch>
2026-09-09 20:00:42 +0200

    feat: implement simple BootNotificationResponse

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
-use futures_util::{StreamExt, TryStreamExt, future};
+use chrono::Local;
+use futures_util::{SinkExt, StreamExt};
+use rust_ocpp::v2_0_1::messages::boot_notification::{
+    BootNotificationRequest, BootNotificationResponse,
+};
+use serde_json::{Value, json};
 use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::tungstenite::{Message, Utf8Bytes};
+
+struct CallError {
+    code: String,
+    description: String,
+}
 
 #[tokio::main]
 async fn main() {
@@ -26,9 +37,104 @@ async fn accept_connection(stream: TcpStream) {
 
     println!("New WebSocket connection: {}", addr);
 
-    let (write, read) = ws_stream.split();
-    read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
-        .forward(write)
-        .await
-        .expect("Failed to forward messages");
+    let (mut write, mut read) = ws_stream.split();
+
+    while let Some(message) = read.next().await {
+        match message {
+            Ok(msg) => {
+                if !msg.is_text() {
+                    continue;
+                }
+
+                match msg.to_text() {
+                    Ok(msg_str) => match serde_json::from_str::<Value>(msg_str) {
+                        Ok(request) => {
+                            let unique_id = request.get(1).unwrap().as_str().unwrap();
+                            let action = request.get(2).unwrap().as_str().unwrap();
+
+                            match action {
+                                "BootNotification" => {
+                                    match handle_boot_notification_request(&request) {
+                                        Ok(response) => {
+                                            let json_string =
+                                                serde_json::to_value(&response).unwrap();
+                                            let response_array = json!([
+                                                3,
+                                                unique_id,
+                                                "BootNotificationResponse",
+                                                json_string
+                                            ])
+                                            .to_string();
+
+                                            println!(
+                                                "Sending response to {}: {}",
+                                                action, response_array
+                                            );
+
+                                            write
+                                                .send(Message::Text(Utf8Bytes::from(
+                                                    response_array,
+                                                )))
+                                                .await
+                                                .unwrap();
+                                        }
+                                        Err(e) => {
+                                            let response_array =
+                                                json!([4, unique_id, e.code, e.description])
+                                                    .to_string();
+
+                                            println!(
+                                                "Sending response to {}: {}",
+                                                action, response_array
+                                            );
+
+                                            write
+                                                .send(Message::Text(Utf8Bytes::from(
+                                                    response_array,
+                                                )))
+                                                .await
+                                                .unwrap();
+                                        }
+                                    }
+                                }
+                                _ => println!("Received unknown request of type '{action}'"),
+                            }
+                        }
+                        Err(e) => println!("Failed to parse message '{msg_str}': {e}"),
+                    },
+                    Err(e) => {
+                        println!("Failed to parse websocket message to string: {e}")
+                    }
+                }
+            }
+            Err(e) => println!("Error: {e}"),
+        }
+    }
+}
+
+fn handle_boot_notification_request(
+    request: &Value,
+) -> Result<BootNotificationResponse, CallError> {
+    match request.get(3) {
+        Some(payload) => {
+            match serde_json::from_value::<BootNotificationRequest>(payload.to_owned()) {
+                Ok(_payload) => Ok(BootNotificationResponse {
+                    current_time: Local::now().to_utc(),
+                    interval: 5,
+                    status: rust_ocpp::v2_0_1::enumerations::registration_status_enum_type::RegistrationStatusEnumType::Accepted,
+                    status_info: None
+                }),
+                Err(e) => {
+                    Err(CallError {
+                        code: String::from("FormationViolation"),
+                        description: String::from("Invalid payload format: ") + &e.to_string()
+                    })
+                }
+            }
+        }
+        None => Err(CallError {
+            code: String::from("FormationViolation"),
+            description: String::from("Missing payload"),
+        }),
+    }
 }
