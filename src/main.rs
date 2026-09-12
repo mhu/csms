9f40bddb00364b40344c42cc1e5be934a1c0592b
mhu <marcel.huismann@protonmail.ch>
2026-09-12 20:51:38 +0200

    refactor: move message parsing logic to its own function

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,15 @@ use serde_json::{Value, json};
 use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::{
     WebSocketStream,
-    tungstenite::{Message, Utf8Bytes},
+    tungstenite::{Error, Message, Utf8Bytes},
 };
+
+struct Call {
+    message_type_id: u8,
+    message_id: String,
+    action: String,
+    payload: Value,
+}
 
 enum CallErrorCode {
     FormationViolation,
@@ -57,90 +64,100 @@ async fn accept_connection(stream: TcpStream) {
     let (mut write, mut read) = ws_stream.split();
 
     while let Some(message) = read.next().await {
-        match message {
-            Ok(msg) => {
-                if !msg.is_text() {
-                    continue;
-                }
-
-                match msg.to_text() {
-                    Ok(msg_str) => match serde_json::from_str::<Value>(msg_str) {
-                        Ok(request) => {
-                            let message_id = request.get(1).unwrap().as_str().unwrap();
-                            let action = request.get(2).unwrap().as_str().unwrap();
-
-                            match action {
-                                "BootNotification" => {
-                                    match handle_boot_notification_request(&request) {
-                                        Ok(response) => {
-                                            write_call_result(&mut write, message_id, response)
-                                                .await;
-                                        }
-                                        Err(e) => {
-                                            write_call_error(&mut write, message_id, e).await;
-                                        }
-                                    }
-                                }
-                                _ => {
-                                    write_call_error(
-                                        &mut write,
-                                        message_id,
-                                        CallError {
-                                            code: CallErrorCode::NotImplemented,
-                                            description: format!(
-                                                "Action '{action}' is not implemented"
-                                            ),
-                                        },
-                                    )
-                                    .await;
-                                }
-                            }
-                        }
-                        Err(e) => {
-                            // TODO: Can we send a `CallError` message here, since the message ID can not be parsed?
-                            println!("Failed to parse message '{msg_str}': {e}")
-                        }
-                    },
-                    Err(e) => {
-                        println!("Failed to parse websocket message to string: {e}")
+        match parse_message(message) {
+            Ok(call) => match call.action.as_str() {
+                "BootNotification" => match handle_boot_notification_request(call.payload) {
+                    Ok(response) => {
+                        write_call_result(&mut write, call.message_id, response).await;
                     }
+                    Err(e) => {
+                        write_call_error(&mut write, call.message_id, e).await;
+                    }
+                },
+                _ => {
+                    let action = call.action;
+
+                    write_call_error(
+                        &mut write,
+                        call.message_id,
+                        CallError {
+                            code: CallErrorCode::NotImplemented,
+                            description: format!("Action '{action}' is not implemented"),
+                        },
+                    )
+                    .await;
                 }
+            },
+            Err(e) => {
+                println!("Failed to read websocket message: {e}");
             }
-            Err(e) => println!("Error: {e}"),
         }
     }
 }
 
-fn handle_boot_notification_request(
-    request: &Value,
-) -> Result<BootNotificationResponse, CallError> {
-    match request.get(3) {
-        Some(payload) => {
-            match serde_json::from_value::<BootNotificationRequest>(payload.to_owned()) {
-                Ok(_payload) => Ok(BootNotificationResponse {
-                    current_time: Local::now().to_utc(),
-                    interval: 5,
-                    status: rust_ocpp::v2_0_1::enumerations::registration_status_enum_type::RegistrationStatusEnumType::Accepted,
-                    status_info: None
-                }),
-                Err(e) => {
-                    Err(CallError {
-                        code: CallErrorCode::FormationViolation,
-                        description: String::from("Invalid payload format: ") + &e.to_string()
-                    })
+fn parse_message(message: Result<Message, Error>) -> Result<Call, String> {
+    match message {
+        Ok(msg) => {
+            if msg.is_empty() {
+                Err(String::from("Empty message"))
+            } else if !msg.is_text() {
+                Err(String::from("Message is not text"))
+            } else {
+                match msg.to_text() {
+                    Ok(msg_str) => match serde_json::from_str::<Value>(msg_str) {
+                        Ok(request) => {
+                            // TODO: Improve parsing of `String` values
+                            let message_type_id: u8 =
+                                serde_json::from_value(request.get(0).unwrap().to_owned()).unwrap();
+                            let message_id: String =
+                                serde_json::from_value(request.get(1).unwrap().to_owned()).unwrap();
+                            let action: String =
+                                serde_json::from_value(request.get(2).unwrap().to_owned()).unwrap();
+                            let payload = request.get(3).unwrap().to_owned();
+
+                            let call = Call {
+                                message_type_id,
+                                message_id,
+                                action,
+                                payload,
+                            };
+
+                            if call.message_type_id != 2 {
+                                Err(format!("Wrong message type: {message_type_id}"))
+                            } else {
+                                Ok(call)
+                            }
+                        }
+                        Err(e) => Err(format!("Failed to parse JSON: {e}")),
+                    },
+                    Err(e) => Err(format!("Failed transform input to text: {e}")),
                 }
             }
         }
-        None => Err(CallError {
-            code: CallErrorCode::FormationViolation,
-            description: String::from("Missing payload"),
+        Err(e) => Err(format!("Received no message: {e}")),
+    }
+}
+
+fn handle_boot_notification_request(payload: Value) -> Result<BootNotificationResponse, CallError> {
+    match serde_json::from_value::<BootNotificationRequest>(payload) {
+        Ok(_payload) => Ok(BootNotificationResponse {
+            current_time: Local::now().to_utc(),
+            interval: 5,
+            status: rust_ocpp::v2_0_1::enumerations::registration_status_enum_type::RegistrationStatusEnumType::Accepted,
+            status_info: None
         }),
+        Err(e) => {
+            Err(CallError {
+                code: CallErrorCode::FormationViolation,
+                description: String::from("Invalid payload format: ") + &e.to_string()
+            })
+        }
     }
 }
 
 async fn write_call_result(
     write: &mut SplitSink<WebSocketStream<TcpStream>, Message>,
-    call_unique_id: &str,
+    call_unique_id: String,
     payload: BootNotificationResponse, // TODO: Allow for more response payloads to be used
 ) {
     let response_type = String::from("BootNotificationResponse"); // TODO: Determine response type based on payload
@@ -155,7 +172,7 @@ async fn write_call_result(
 
 async fn write_call_error(
     write: &mut SplitSink<WebSocketStream<TcpStream>, Message>,
-    call_unique_id: &str,
+    call_unique_id: String,
     error: CallError,
 ) {
     let response_array =
